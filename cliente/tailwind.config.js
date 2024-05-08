00bafb563b67bb15e61dda963471d61ba945e820
Juan Pablo Valdez <cv.juanp@gmail.com>
2024-05-08 08:39:57 -0700

    Agregado colores personalizados a tailwind.config

--- cliente/tailwind.config.js
+++ cliente/tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pktVerde: "#3dae2b",
+        pktAzul: "#165090",
+        pktCafé: "#bd8e68",
+      },
+    },
   },
   plugins: [],
 };
