c922d17e45b967a627e1efc7f37b610634704ccf
Juan Pablo Valdez <cv.juanp@gmail.com>
2024-05-09 12:11:46 -0700

    Agregado servicio  para solicitar agenda del contacto

--- servidor/src/Rutas/Agenda.js
+++ servidor/src/Rutas/Agenda.js
@@ -2,7 +2,30 @@ const express = require("express");
 const rutaAgenda = express.Router();
 const AgendaModelo = require("../Modelos/Agenda");
 const AutentificarJWT = require("../middleware/AutentificarJWT");
-const ValidarNuevoContacto = require("../Validación/Contacto");
+
+/**
+ * Obtiene la agenda del usuario
+ * Debe ser una petición GET
+ * Debe contener un encabezado de autentificación
+ * @param {headers: {Authorization: string}}
+ */
+rutaAgenda.get("/", AutentificarJWT, async (req, res) => {
+  //Extraemos el id del token
+  const id = req.user._id;
+  try {
+    const agenda = await AgendaModelo.findById(id);
+    res.json(agenda);
+  } catch (error) {
+    res.status(500).json({ message: "Error Interno" });
+  }
+});
+
+/**
+ * Crea un nuevo contacto en la agenda del usuario
+ * Debe ser una petición POST
+ * @param {nombre: string, apellidos: string, correo: string, teléfonos: string[],
+ *  direcciones: Dirección[{calle: string, número: string, ciudad: string}] }
+ */
 
 rutaAgenda.post("/", AutentificarJWT, async (req, res) => {
   //Extraemos el id del token
@@ -15,6 +38,7 @@ rutaAgenda.post("/", AutentificarJWT, async (req, res) => {
   const direcciones = req.body.direcciones;
   const nuevoContacto = { nombre, apellido, correo, teléfonos, direcciones };
   //Validamos la petición
+  const ValidarNuevoContacto = require("../Validación/Contacto");
   try {
     ValidarNuevoContacto.parse(nuevoContacto);
   } catch (error) {
